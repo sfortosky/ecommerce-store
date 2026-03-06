b11a22eb11abfaa3c5bc6980c08667eb853904b1
Sean Fortosky <fortoskysean@gmail.com>
2026-03-05 21:08:47 -0600

    Add user model and db connection functionality

--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -7,7 +7,13 @@ from sqlalchemy.orm import sessionmaker
 env_path = Path(__file__).resolve().parent.parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL not found. Check .env file location.")
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from app.database import Base, engine
+
+from app.models.user_model import User
+
+# Create all db tables
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
